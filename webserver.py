303e6816d73fc4dead8a5ae886f67c6fa4ad0cc8
Arnav Kumar Mandal <arnav.mandal1234@gmail.com>
2018-06-12 13:08:20 +0530

    Small Modification.

--- webserver.py
+++ webserver.py
@@ -2,7 +2,7 @@ import socket
 import re
 
 host = ""
-port = 8000	w
+port = 8000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((host, port))
